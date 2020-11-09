9ce1ae936b6cc549d59b329b6a16549b584f217f
Russell Carpenter <rcarpenter24@biomedscienceacademy.org>
2020-11-09 22:44:14 +0000

    the whole code. Everything was wrong so I fixed the print and variables.

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Rusty Carpenter
+Period/Core: 3
 
 
 """
+
+action = input("state an action word: ")
+
+action2 = input("state an action word(ing): ")
+action3 = input("state an action word(ing): ")
+noun = input("State a noun: ")
+noun2 = input("state a noun: ")
+noun3 = input("State a noun: ")
+action4 = input("state an action word: ")
+noun4 = input("State a noun: ")
+
+
+print (f"One day I was {action} through the park when I noticed a man {action2} his kids.")
+print (f"I decided not to get in volved so I started {action3} home. When I got home, my {noun} called and told me to throw my {noun2} away.")
+print (f"As I walked to the trashcan, my {noun3} stopped me and asked, What are you doing?")
+print (f"i said, Im taking out the {noun2}. then my {noun3} said, do you wanna {action4} to my {noun4}?")
